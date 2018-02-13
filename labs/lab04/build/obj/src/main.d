c7e81ca0a594ae6595ab089259e7ad5d7262b9b1
Rui <zuoxx067@umn.edu>
2018-02-13 11:21:42 -0600

    1

--- labs/lab04/build/obj/src/main.d
+++
@@ -1,8 +0,0 @@
-../build/obj/src/main.o: main.cc ../src/robot_viewer.h \
- ../src/robot_land.h ../src/robot.h
-
-../src/robot_viewer.h:
-
-../src/robot_land.h:
-
-../src/robot.h:
--- labs/lab04/build/obj/src/robot.d
+++
@@ -1,3 +0,0 @@
-../build/obj/src/robot.o: robot.cc robot.h
-
-robot.h:
