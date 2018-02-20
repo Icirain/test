79730479fac11e76a4efa55d9509785fe45bac8d
Rui <zuoxx067@umn.edu>
2018-02-19 18:49:24 -0600

    remove ht

---
+++ labs/lab05/docs/html/search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['set_5ftime',['set_time',['../classrobot__land.html#a9b759efccbcae2be07ead240caec14cf',1,'robot_land']]]
+];
---
+++ labs/lab05/docs/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "adgmorst",
+  1: "or",
+  2: "mor",
+  3: "adgos",
+  4: "mt"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Files",
+  3: "Functions",
+  4: "Pages"
+};
+
