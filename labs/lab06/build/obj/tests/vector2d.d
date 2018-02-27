8c398889972287cb2f8e959096b5eb38ddbb3986
Rui <zuoxx067@umn.edu>
2018-02-26 22:02:03 -0600

    delete

--- labs/lab06/build/obj/tests/vector2d.d
+++
@@ -1,3 +0,0 @@
-../build/obj/tests/vector2d.o: ../src/vector2d.cc ../src/vector2d.h
-
-../src/vector2d.h:
--- labs/lab06/build/obj/tests/vector2d_test.d
+++
@@ -1,3 +0,0 @@
-../build/obj/tests/vector2d_test.o: vector2d_test.cc ../src/vector2d.h
-
-../src/vector2d.h:
