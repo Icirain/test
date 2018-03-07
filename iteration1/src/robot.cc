b604f74f59d1f5b3cf30d7406f81be940c05ad9d
Rui <zuoxx067@umn.edu>
2018-03-06 19:45:37 -0600

    add some comment

--- iteration1/src/robot.cc
+++ iteration1/src/robot.cc
@@ -37,6 +37,7 @@ void Robot::TimestepUpdate(unsigned int dt) {
     mercy_time_ -= dt;
     // set_color(mercy_time_%2 == 0? ROBOT_COLOR:DEFAULT_COLOR);
   }
+  // Change color for robot in mercy_time_
   if (mercy_time_ % 2 == 0)set_color(ROBOT_COLOR);
   else
   set_color(MERCY_COLOR);
@@ -62,9 +63,11 @@ void Robot::HandleCollision(EntityType object_type, ArenaEntity * object) {
     int lives_remained = get_lives();
     set_lives(lives_remained <= 0? 0 : lives_remained - 1);
     return;
+    // lives will be deducted iff robot is out of mercy_time_ when mercy_time_ is 0
   } else if (object_type == kObstacle && mercy_time_ == 0) {
     int lives_remained = get_lives();
     set_lives(lives_remained <= 0? 0 : lives_remained - 1);
+    // When collided with an obstacle , a mercy time will be provided in about 2s
     mercy_time_ = 36;
   }
   motion_handler_.set_velocity(0, 0);
